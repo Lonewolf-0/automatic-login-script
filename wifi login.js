827ed55a154093cfeea16b1eda01d5345e32bcca
Aryan Tyagi <aryan.tyagi202@gmail.com>
2022-11-25 09:49:14 +0530

    Add files via upload

---
+++ wifi login.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://192.168.1.254:8090/httpclient.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=1.254
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    document.querySelector("#username").setAttribute("value","username");
+    document.querySelector("#password").setAttribute("value","password");
+    document.querySelector("#loginbutton").click();
+})();
